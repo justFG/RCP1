ff9998e99240bd955862022dd52f1010dfa74cd6
Farouk <127602912+justFG@users.noreply.github.com>
2026-03-18 18:59:08 +0100

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Homepage'
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
