4ea66605858de9f451f37512ed363587eae9c181
yacine01-fg <yacinedjoouaher7@gmail.com>
2026-03-22 12:52:19 +0100

    add product details page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,7 @@ const ProductCard = ({ product }) => {
   const navigate = useNavigate();
 
   const handleViewDetails = () => {
-    navigate(`/product/{product.id}da`);
+    navigate(`/products/${product.id}`);
   };
 
   return (
@@ -12,40 +12,40 @@ const ProductCard = ({ product }) => {
                 shadow-sm hover:shadow-md hover:scale-[1.018]
                 transition-all duration-300 ease-out">
 
-  {/* Image */}
-  <div className="bg-gray-50 p-3 rounded-xl mb-4">
-    <img
-      src={product.image}
-      alt={product.name}
-      className="w-44 h-44 mx-auto rounded-lg object-cover"
-    />
-  </div>
+      {/* Image */}
+      <div className="bg-gray-50 p-3 rounded-xl mb-4">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="w-44 h-44 mx-auto rounded-lg object-cover"
+        />
+      </div>
 
-  {/* Title */}
-  <h3 className="text-lg font-semibold text-gray-900 mb-1">
-    {product.name}
-  </h3>
+      {/* Title */}
+      <h3 className="text-lg font-semibold text-gray-900 mb-1">
+        {product.name}
+      </h3>
 
-  {/* Specs */}
-  <p className="text-sm text-gray-500 mb-3">
-    {product.shortDescription}
-  </p>
+      {/* Specs */}
+      <p className="text-sm text-gray-500 mb-3">
+        {product.shortDescription}
+      </p>
 
-  {/* Price */}
-  <p className="text-xl font-bold text-gray-900 mb-4">
-    {product.price.toLocaleString()} da
-  </p>
+      {/* Price */}
+      <p className="text-xl font-bold text-gray-900 mb-4">
+        {product.price.toLocaleString()} da
+      </p>
 
-  {/* Button */}
-  <button
-    onClick={handleViewDetails}
-    className="mt-auto bg-purple-600/90 text-white py-2.5 rounded-lg
+      {/* Button */}
+      <button
+        onClick={handleViewDetails}
+        className="mt-auto bg-purple-600/90 text-white py-2.5 rounded-lg
                hover:bg-purple-700 transition-colors"
-  >
-    View Details
-  </button>
+      >
+        View Details
+      </button>
 
-</div>
+    </div>
   );
 };
 
