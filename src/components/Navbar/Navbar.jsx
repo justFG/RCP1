522b2eb03779a0b93ff7311c4802a795cdad8b21
Farouk <127602912+justFG@users.noreply.github.com>
2026-03-25 13:11:19 +0100

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { useState } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ hp, pr,ab }) => {
   const [Menu, setMenu] = useState(false);
-  
+    const navigate = useNavigate();
+      const homepagenav = () => {
+    navigate(`/`);
+
+  };
   return (
-    <>
+    <div className="z-50 relative">
       <nav>
-        <div className={`hidden md:flex h-18 items-center text-l gap-3 pl-15 pr-15 justify-between shadow ${hp ? "bg-[#F5F5F5]": "bg-[#001433]"}`}>
-          <img src={hp ? "LogoC.png" : "Logo.png"}  width='13%'/>
-          <div className={`flex gap-5 text-white font-[Impact] text-xl`}>
+        <div className={`hidden md:flex h-18 items-center text-l gap-3 pl-15 pr-15 justify-between shadow ${hp ? "bg-[#001433]" : "bg-[#F5F5F5]"}`}>
+          <img className="cursor-pointer" src={hp ? "Logo.png" : "LogoC.png"} width='13%' onClick={homepagenav}/>
+          <div className={`flex gap-5 ${hp ? "text-white" : "text-[#000000]"} font-[Impact] text-xl`}>
           <a className={`transition duration-200 p-2 rounded-md ${hp ? "text-[#19CEE4]" : " hover:text-[#19CEE4]"}`} href='/'>Home</a>
           <a className={`transition duration-200 p-2 rounded-md ${pr ? "text-[#19CEE4]" : " hover:text-[#19CEE4]"}`} href='/products'>See our products</a>
           <a className={`transition duration-200 p-2 rounded-md ${ab ? "text-[#19CEE4]" : " hover:text-[#19CEE4]"}`} href='/products'>About</a>
@@ -20,18 +25,17 @@ const Navbar = ({ hp, pr,ab }) => {
       </nav>
 
       <nav>
-        <div className={`flex md:hidden h-18 items-center text-l gap-3 justify-between shadow ${hp ? "bg-[#F5F5F5]": "bg-[#001433]"}`}>
-          <img src="Logo.png" width='50%'/>
-          <a className={`mr-6 transition-transform duration-300 text-white ${Menu ? "rotate-90" : "rotate-0"}`} onClick={() => setMenu(!Menu)}><MenuIcon/></a>
+        <div className={`flex md:hidden h-18 items-center text-l gap-3 justify-between shadow ${hp ? "bg-[#001433]" : "bg-[#F5F5F5]"}`}>
+          <img src={hp ? "Logo.png" : "LogoC.png"} width='50%' onClick={homepagenav}/>
+          <a className={`mr-6 transition-transform duration-30 ${hp ? "text-white" : "text-[#000000]"} ${Menu ? "rotate-90" : "rotate-0"}`} onClick={() => setMenu(!Menu)}><MenuIcon/></a>
         </div>
-          <div className={`flex flex-col text-white bg-[#001433] px-4 gap-2 font-[Impact] text-xl duration-300 ease-in-out overflow-hidden ${Menu ? "max-h-45 py-2" : "max-h-0 py-0"}`}>
+          <div className={`flex flex-col ${hp ? "text-white" : "text-[#000000]"} ${hp ? "bg-[#001433]" : "bg-[#F5F5F5]"} px-4 gap-2 font-[Impact] text-xl duration-300 ease-in-out overflow-hidden ${Menu ? "max-h-45 py-2" : "max-h-0 py-0"}`}>
             <a className={`p-2 rounded-md ${hp ? "text-[#19CEE4]" : "hover:text-[#19CEE4]"}`} href="/">Home</a>
             <a className={`p-2 rounded-md ${pr ? "text-[#19CEE4]" : "hover:text-[#19CEE4]"}`} href="/products">See our products</a>
             <a className={`p-2 rounded-md ${ab ? "text-[#19CEE4]" : "hover:text-[#19CEE4]"}`} href="/about">About</a>
           </div>
       </nav>
-
-    </>
+    </div>
   );
 };
 
